332a43005c3b27937fcf0529314ec069d60ae0ff
nyirurugo omar <nyirurugoomar@gmail.com>
2022-08-06 15:07:59 +0200

    add section

--- src/components/Home.js
+++ src/components/Home.js
@@ -5,7 +5,66 @@ import Section from "./Section"
 function Home() {
   return (
     <Container>
-        <Section/>
+        <Section
+               title='Model S'
+               description='Order Online for Touchless Deliver'
+               backgroundImg='model-s.jpg'
+               leftBtnText='Custom Order'
+               rightBtnText='Existing Inventory'
+        
+        />
+        <Section
+               title='Model Y'
+               description='Order Online for Touchless Deliver'
+               backgroundImg='model-y.jpg'
+               leftBtnText='Custom Order'
+               rightBtnText='Existing Inventory'
+        
+        />
+        <Section
+               title='Model 3'
+               description='Order Online for Touchless Deliver'
+               backgroundImg='model-3.jpg'
+               leftBtnText='Custom Order'
+               rightBtnText='Existing Inventory'
+        
+        />
+        <Section
+                title='Model X'
+                description='Order Online for Touchless Deliver'
+                backgroundImg='model-x.jpg'
+                leftBtnText='Custom Order'
+                rightBtnText='Existing Inventory'
+        
+        />
+
+        <Section
+              title='Lowest Cost Solar Panels in American'
+              description='Money-back guarantee'
+              backgroundImg='solar-panel.jpg'
+              leftBtnText='Order now'
+              rightBtnText='Learn more'
+        
+        />
+
+        <Section
+              title='Solar for New Roofs'
+              description='Solar Roof Costs Less Than a New Roof Plus Solar Panels'
+              backgroundImg='solar-roof.jpg'
+              leftBtnText='Order now'
+              rightBtnText='Learn more'
+        
+        />
+
+        <Section
+              title='Accessories'
+              description=''
+              backgroundImg='accessories.jpg'
+              leftBtnText='Shop now'
+              
+        
+        />
+        
     </Container>
   )
 }
